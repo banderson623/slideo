5b16aa5bde24de1d75ff23d6f269751ce8e0e7db
Brian Anderson <brian_anderson@brian-andersons-imac-2.local>
2008-11-05 07:18:43 -0600

    custom splash image

--- javascripts/slideo.js
+++ javascripts/slideo.js
@@ -2,13 +2,11 @@
 // 2008 - A little favor for BILD friends (Levi and Nate)
 // For: http://testing.antiochmanifesto.org
 
-slideo_config = {
-
-}
-
 var slideo_global_player_object;
 
 Slideo = Class.create({
+  
+    // User configurable for slideo, these should be set as the second argument in the constructor
     config: {
         time_in_seconds:0,
         current_slide: 0,
@@ -19,20 +17,27 @@ Slideo = Class.create({
         slide_img: "slideo_slides_img",
         slide_cache_img: "slideo_cache_img",
         video_div: "slideo_video",
-        loading_div: "slideo_loading",
-        loading_content: "<p>Loading</p>",
+        message_div: "slideo_message",
+        video_splash_div: "slideo_video_splash",
+        loading_content: "<p class=\"loading\">Loading</p>",
+        video_unavailable: '<p class="unavailable error">Video is not available</p>',
         flow_player_swf: "swf/FlowPlayerDark.swf",
         slide_url: null,
         timecode_url: null,  
         video_url: null,
-        splash_url: null
+        splash_url: null,
+        cache_next_image_after: 8.0, // delay in seconds
+        experimental_load_percent_before_playback_can_start: 1.1
     },
+    
+    
     // Private status state (please don't monkey with)
     status: {
       flow_player_ready: false,
       state: "uninitialized",
       onLoadBeginCalls: 0,
       time_check_timer: null,
+      loading_timer: null,
       playing: false,
       slide_number: -1,
       slide_index: -1,
@@ -41,22 +46,14 @@ Slideo = Class.create({
       current_time: -1,
       cached_next_slide: false,
       working_cache: null,
-      playback_can_start: false
+      playback_can_start: false,
+      started_loading_at: 0
     },
     
     flow_player: null,
     slides: [],
     container_element: null,
-    
-    set_state: function(new_state){
-        this.log("Setting state to: " + new_state)
-        return this.status.state = new_state;
-    },
-    
-    state: function(){
-        return this.status.state;
-    },
-    
+      
     initialize: function(el, configuration){
         this.log("Initalizing");
         if(valueOf(arguments[1]) != undefined ) {
@@ -68,7 +65,6 @@ Slideo = Class.create({
         this.buildInternalHtmlElements();
         this.loadFlowPlayer();
         this.installPlayerReadyCallback();
-        this.set_state("initialized");
         return this;
     },
     
@@ -76,24 +72,24 @@ Slideo = Class.create({
         this.log('building slideo elements in ' + this.container_element.id);
         this.video_element = new Element('div', { 'id': this.config.video_div});
         this.slide_element = new Element('div', { 'id': this.config.slide_div});
-        this.slide_image_element = new Element('img', { 'id': this.config.slide_img});
+        this.video_splash_element = new Element('img', { 'id': this.config.video_splash_div});
+        
+        this.slide_image_element = new Element('img', { 'id': this.config.slide_img}).hide();
         this.slide_cache_img_element = new Element('img', { 'id': this.config.slide_cache_img, "style":"display:none;"});
-        this.loading_element = new Element('div', { 'id': this.config.loading_div}).update(this.config.loading_content);
+        this.message_element = new Element('div', { 'id': this.config.message_div}).hide();
 
         this.container_element.insert({top: this.video_element});
         this.container_element.insert({bottom: this.slide_element});
+        this.container_element.insert({bottom: this.video_splash_element});
+
         this.slide_element.insert(this.slide_image_element);
         this.slide_element.insert(this.slide_cache_img_element);
         
-        this.container_element.insert({bottom: this.loading_element});
-        
-        
-        if(this.config.show_slide_progress_bar){
-          
-        }
+        this.container_element.insert({bottom: this.message_element});
     },
     
     loadFlowPlayer: function(){
+        this.status.started_loading_at = (new Date).getTime()
         window.slideo_api = this.flow_player = window.flashembed(this.config.video_div,  
            { src: this.config.flow_player_swf,
              width: this.config.width, 
@@ -103,8 +99,7 @@ Slideo = Class.create({
     
     // Responsible for returning a JS Object/Hash
     // for all of the flow player config
-    flowPlayerConfig: function(){
-      
+    flowPlayerConfig: function(){  
       return {
         config: {   
            autoPlay: false,
@@ -123,14 +118,25 @@ Slideo = Class.create({
            initialVolumePercentage: 100,
            timeDisplayFontColor: 0x999999,
            showMenu: false,   				
-           splashImageFile: this.config.splash_url,
+           // splashImageFile: this.config.splash_url,
            videoFile: this.config.video_url
          }
       }
     },
     
+    message: function(text_for_message){
+      this.log("Showing message: " + text_for_message)
+      this.message_element.update(text_for_message).show();
+    },
+    
+    hideMessage: function(){
+      this.message_element.hide();
+    },
+    
     log:function(text){
-        console.log(text);
+      try{ console.log(text);}
+      catch(err){//logging not supported
+      }
     },
     
   
@@ -140,41 +146,63 @@ Slideo = Class.create({
         this.status.flow_player_ready = true;
         this.installCallBacks();
       }.bind(this)
+      
+
+      var splash_url = this.config.splash_url
+      console.log(" displaying splash: " + splash_url);
+      this.video_splash_element.src = splash_url;
+      // this.video_splash_element.hide();
+      this.showLoading()
+      
     },
     
     // 
     canStartPlayback: function(){
       this.log("Playback can start now (loaded: " + this.flow_player.getPercentLoaded() +  "%) ");
-      this.loading_element.hide();
+      this.hideMessage();
+      this.video_splash_element.hide();
       this.flow_player.DoPlay();
+      this.log(this.flow_player.getTime());
       this.status.playback_can_start = true;
+      this.log("It took " + ((new Date()).getTime() - this.status.started_loading_at)/1000 + " seconds to load before it could play");
+    },
+    
+    showLoading: function(){
+      var percent_load = (this.flow_player.getPercentLoaded() / this.config.experimental_load_percent_before_playback_can_start) * 100;
+      if (percent_load >= 100) { percent_load = 100; }
+      if (percent_load < 0) { percent_load = 0; }
+      // 
+      // console.log("loading..." + percent_load);
+      // 
+      this.message(this.config.loading_content);
+      try{ this.message_element.down('p').insert({bottom: " (" + Math.round(percent_load) + "%)"});}
+      catch(err) {}
+      // 
+      if(!this.status.playback_can_start){
+        this.status.show_loading_timer = setTimeout(function(){this.showLoading();}.bind(this), 250);
+      } else {
+        this.hideMessage();
+      }
+        
     },
     
     installCallBacks: function(){
       window.onLoadBegin = function(clip){
          // this.log("onLoadBegin");
          this.status.onLoadBeginCalls++;
-         // this.log(this.status.onLoadBeginCalls);
+         this.log(this.status.onLoadBeginCalls);
          // window.alert(this.status.onLoadBeginCalls);
-         if(this.status.onLoadBeginCalls >= 4 && this.status.playback_can_start == false) this.canStartPlayback();
+         if(this.status.onLoadBeginCalls >= 3 && this.status.playback_can_start == false) this.canStartPlayback();
          
       }.bind(this);
       
-      window.onStartBuffering = function(clip){
-         // this.log("onStartBuffering");
-         this.scrubbing();
-      }.bind(this);
+      window.onStartBuffering = function(clip){ this.scrubbing(); }.bind(this);
+      window.onBufferFlush =    function(clip){ this.log("onBufferFlush"); }.bind(this);
+      window.onBufferFull =     function(){ this.log("onBufferFull"); }.bind(this);
+      window.onStreamNotFound = function(){ this.message(this.config.video_unavailable); }.bind(this);
       
-      window.onBufferFlush = function(clip){
-        this.log("onBufferFlush");
-      }.bind(this);
-      
-      window.onBufferFull = function(){
-         this.log("onBufferFull");
-      }.bind(this);
-      
-      window.onResume = window.onPlay = function(clip){ this.playing(clip);}.bind(this);
-      window.onStop = window.onPause = function(clip){ this.stopped(clip);}.bind(this)
+      window.onResume = window.onPlay = function(clip){ this.playing(clip); }.bind(this);
+      window.onStop = window.onPause =  function(clip){ this.stopped(clip); }.bind(this)
       
     },    
     playing: function(clip){
@@ -217,10 +245,9 @@ Slideo = Class.create({
     },
     
     setSlideForTime: function(time){
-      // this.log("Checking if I can set a new slide: next slide at: " + this.status.next_slide_at + " seconds.");
       if(time > this.status.next_slide_at){
         this.slide_index = this.getSlideIndexForSecond(time)
-        // this.log("Switching to slide " + this.slides[this.slide_index].slide_number);
+        this.log("Switching to slide " + this.slides[this.slide_index].slide_number);
         this.showSlideAtIndex(this.slide_index);
         if(this.status.max_index > this.slide_index) {
           this.status.next_slide_at = this.slides[this.slide_index+1].second;
@@ -248,6 +275,7 @@ Slideo = Class.create({
     showSlideAtIndex: function(slide_index){
       var slide_url = this.config.slide_url + "/" + this.slides[slide_index].fileName();
       this.slide_image_element.src = slide_url;
+      this.slide_image_element.show();
       this.cacheSlideAtIndex(slide_index + 1);
       this.log("loading slide; " + slide_url);
     },
@@ -258,7 +286,7 @@ Slideo = Class.create({
         this.status.working_cache = setTimeout(function(){
           this.log('cached slide ' + this.slides[index].slide_number);
           this.slide_cache_img_element.src = this.config.slide_url + "/" + this.slides[index].fileName();
-        }.bind(this), 1000);
+        }.bind(this), (this.config.cache_next_image_after * 1000));
       }
     },
     
@@ -293,7 +321,8 @@ Slideo = Class.create({
               this.setSlideTimingsFromArrayOfTimeCodes(transport.responseText.split("\n"));
           }.bind(this),
           onFailure: function(transport){
-              this.log("something wasn't happy!");
+              this.log("Could not load the slide timings");
+              this.message("<p>Could not load slide timings</p>")
           }
         });
     },
